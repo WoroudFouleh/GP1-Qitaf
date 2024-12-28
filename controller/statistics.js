3caa2044705580fee78288ead71476e7ea1f1b2d
RaghadMM <s12027617@stu.najah.edu>
2024-12-29 01:28:49 +0200

    statistics final

--- controller/statistics.js
+++ controller/statistics.js
@@ -2,13 +2,13 @@ const Product = require('../model/product');
 const ProductionLine = require('../model/productionLine'); // Path to your ProductionLine model
 const User = require('../model/user');
 const Land = require('../model/land'); // adjust the path if needed
-
+const DeliveryMan = require('../model/deliveryMan'); // adjust the path if needed
 exports.getOverallStatistics = async (req, res) => {
     try {
-        const usersCount = await User.countDocuments({ userType: 2 }); // Count users with type = 2
-        const landownersCount = await User.countDocuments({ userType: 1 }); // Count users with type = 1
+        const usersCount = await User.countDocuments({ userType: 1 }); // Count users with type = 2
+        const landownersCount = await User.countDocuments({ userType: 2 }); // Count users with type = 1
         
-      //const deliveryMenCount = await DeliveryMan.countDocuments();
+      const deliveryMenCount = await DeliveryMan.countDocuments();
       const landsCount = await Land.countDocuments();
       const productsCount = await Product.countDocuments();
       const productionLinesCount = await ProductionLine.countDocuments();
@@ -16,7 +16,7 @@ exports.getOverallStatistics = async (req, res) => {
       res.json({
         users: usersCount,
         landowners: landownersCount,
-        //deliveryMen: deliveryMenCount,
+        deliveryMen: deliveryMenCount,
         lands: landsCount,
         products: productsCount,
         productionLines: productionLinesCount,
